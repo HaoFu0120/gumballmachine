18520713d01010e0f17ba5ecd7795cf72b79d689
victory felix <victoryfelix@victorys-mbp.attlocal.net>
2019-09-15 12:57:20 -0700

    add functionality

--- gumball-java-typical/GumballMachine.java
+++ gumball-java-typical/GumballMachine.java
@@ -1,42 +1,57 @@
-public class GumballMachine
-{
+public class GumballMachine {
 
     private int num_gumballs;
     private boolean has_quarter;
+    private int total;
+    private int gumball_cost;
+    private String gumball_type;
 
-    public GumballMachine( int size )
-    {
+    public GumballMachine(int size, int cost, String type) {
         // initialise instance variables
         this.num_gumballs = size;
         this.has_quarter = false;
+        this.total = 0;
+        this.gumball_cost = cost;
+        this.gumball_type = type;
     }
 
-    public void insertQuarter(int coin)
-    {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+    public void insertQuarter(int coin) {
+        if (coin == 25 && this.gumball_cost == 25 && this.gumball_type.equals("quater")) {
+            total += coin;
+            this.has_quarter = true;
+            System.out.println( "A quarter inserted." );
+        } else if (coin == 25 && this.gumball_cost == 50 && this.gumball_type.equals("quater"))  {
+            total += coin;
+            System.out.println( "A quarter inserted." );
+        } else if (this.gumball_cost == 50 && this.gumball_type.equals("any")){
+            total += coin;
+        } else {
+            System.out.println("This machine only accepts quaters, coin ejecting.");
+            System.out.println("Please insert a quarter.");
+        }
     }
     
-    public void turnCrank()
-    {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
+    public void turnCrank() {
+    	if (this.gumball_type.equals("quater") && (this.total >= this.gumball_cost)) {
+    		if (this.num_gumballs > 0) {
     			this.num_gumballs-- ;
     			this.has_quarter = false ;
     			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
     		}
-    		else
-    		{
+    		else {
+                System.out.println( "A quarter inserted." );
     			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
     		}
+    	}  else if (this.total >= this.gumball_cost && this.gumball_type.equals("any")) {
+            if (this.num_gumballs > 0) {
+                this.num_gumballs-- ;
+                System.out.println( "Thanks for your coins.  Gumball Ejected!" ) ;
+            }
+            else {
+                System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+            }
+        } else if ((this.total < this.gumball_cost) && this.gumball_type.equals("any")){
+    		System.out.println( "Not enough coins, please insert more!" ) ;
     	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
     }
 }
