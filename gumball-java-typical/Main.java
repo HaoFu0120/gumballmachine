d399273243891448336d4a5c6d08913f491d5a0c
victory felix <victoryfelix@victorys-mbp.attlocal.net>
2019-09-15 13:00:32 -0700

    uncomment test model

--- gumball-java-typical/Main.java
+++ gumball-java-typical/Main.java
@@ -10,26 +10,26 @@ public class Main {
         m1.turnCrank();
         m1.turnCrank();
 
-//        GumballMachine m2 = new GumballMachine(3, 50, "quater");
-//        System.out.println("GumballMachine 2: ");
-//        m2.insertQuarter(10);
-//        m2.turnCrank();
-//        m2.insertQuarter(25);
-//        m2.insertQuarter(25);
-//        m2.insertQuarter(10);
-//        m2.insertQuarter(10);
-//        m2.turnCrank();
+        GumballMachine m2 = new GumballMachine(3, 50, "quater");
+        System.out.println("GumballMachine 2: ");
+        m2.insertQuarter(10);
+        m2.turnCrank();
+        m2.insertQuarter(25);
+        m2.insertQuarter(25);
+        m2.insertQuarter(10);
+        m2.insertQuarter(10);
+        m2.turnCrank();
 
-//        GumballMachine m3 = new GumballMachine(3, 50, "any");
-//        System.out.println("GumballMachine 3: ");
-//        m3.insertQuarter(10);
-//        m3.insertQuarter(50);
-//        m3.turnCrank();
-//        m3.insertQuarter(25);
-//        m3.insertQuarter(10);
-//        m3.insertQuarter(10);
-//        m3.insertQuarter(10);
-//        m3.turnCrank();
-//        m3.turnCrank();
+        GumballMachine m3 = new GumballMachine(3, 50, "any");
+        System.out.println("GumballMachine 3: ");
+        m3.insertQuarter(10);
+        m3.insertQuarter(50);
+        m3.turnCrank();
+        m3.insertQuarter(25);
+        m3.insertQuarter(10);
+        m3.insertQuarter(10);
+        m3.insertQuarter(10);
+        m3.turnCrank();
+        m3.turnCrank();
     }
 }
